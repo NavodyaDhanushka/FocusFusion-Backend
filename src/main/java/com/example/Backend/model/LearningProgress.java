a9ed2932c5a8348ce7b6a103c0dc8924215f17d8
hazard <thxnujay@gmail.com>
2025-04-26 21:39:00 +0530

    changed relevent comments in learning progress

--- src/main/java/com/example/Backend/model/LearningProgress.java
+++ src/main/java/com/example/Backend/model/LearningProgress.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-//create learning prograss modle
+//create learning prograss model
 
 @Document(collection = "learning_progress")
 @AllArgsConstructor
